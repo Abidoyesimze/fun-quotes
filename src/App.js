1a0221b2479f3107014b80bb1f721cfd506eae7d
Abidoyesimze <similoluwaeyitayoabidoye@gmail.com>
2024-04-04 17:50:52 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from "./components/card";
+import { useState } from 'react';
+import "tailwindcss/tailwind.css";
+
 
 function App() {
+  const [quote, setQuote] = useState({
+    anime: "Hyouka",
+    character: "Eru Chitanda",
+    quote: "I find myself hard to understand sometimes."
+  });
+
+  function getQuote(){
+    fetch("https://animechan.xyz/api/random")
+    .then((res) => res.json())
+    .then((data) => setQuote(data))
+    .catch((error) =>console.log(error.message));
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex justify-center items-center bg-auto bg-center">
+      <div>
+      {quote && <Card quote={quote} />}
+      <button onClick={getQuote} className="bg-green-400 px-5 py-4 mt-5 text-white">Get Quote</button>
+      </div>
     </div>
   );
 }
